b77d3ea57e5ecb94c15daeb83d2f252af7696af8
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-04-23 19:12:32 -0300

    Implemented sendMetrics method

--- src/main/java/io/galeb/hazelcast/EventBus.java
+++ src/main/java/io/galeb/hazelcast/EventBus.java
@@ -3,21 +3,21 @@ package io.galeb.hazelcast;
 import io.galeb.core.controller.EntityController.Action;
 import io.galeb.core.eventbus.Event;
 import io.galeb.core.eventbus.EventBusListener;
-import io.galeb.core.eventbus.EventBusSingletoneProducer;
 import io.galeb.core.eventbus.IEventBus;
 import io.galeb.core.logging.Logger;
 import io.galeb.core.model.Entity;
+import io.galeb.core.model.Metrics;
 
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.enterprise.inject.Default;
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IQueue;
 import com.hazelcast.core.ITopic;
 import com.hazelcast.core.Message;
 import com.hazelcast.core.MessageListener;
@@ -30,26 +30,18 @@ public class EventBus implements MessageListener<Event>, IEventBus {
 
     private static final HazelcastInstance HAZELCAST_INSTANCE = Hazelcast.newHazelcastInstance();
 
-    private static final EventBus INSTANCE = new EventBus();
-
     private final Map<String, ITopic<Event>> topics = new HashMap<>();
 
+    private final IQueue<Metrics> queueMetrics = HAZELCAST_INSTANCE.getQueue(Metrics.METRICS_QUEUE);
+
+
     private EventBusListener eventBusListener = EventBusListener.NULL;
-
-    private EventBus() {
-        // singleton
-    }
-
-    @Produces @EventBusSingletoneProducer
-    public EventBus getInstance() {
-        return INSTANCE;
-    }
 
     private ITopic<Event> putAndGetTopic(String topicId) {
         ITopic<Event> topic = topics.get(topicId);
         if (topic==null) {
-            topic = HAZELCAST_INSTANCE.getTopic( topicId );
-            topic.addMessageListener( this );
+            topic = HAZELCAST_INSTANCE.getTopic(topicId);
+            topic.addMessageListener(this);
             topics.put(topicId, topic);
 
             logger.debug(String.format("registered at %s", topicId));
@@ -61,9 +53,7 @@ public class EventBus implements MessageListener<Event>, IEventBus {
     public void publishEntity(final Entity entity, final String entityType, final Action action) {
 
         entity.setEntityType(entityType);
-
         final Event event = new Event(action, entity);
-
         final ITopic<Event> topic = putAndGetTopic(action.toString());
 
         try {
@@ -72,6 +62,11 @@ public class EventBus implements MessageListener<Event>, IEventBus {
             logger.error(e);
         }
 
+    }
+
+    @Override
+    public void sendMetrics(Metrics metrics) throws InterruptedException {
+        queueMetrics.put(metrics);
     }
 
     @Override
