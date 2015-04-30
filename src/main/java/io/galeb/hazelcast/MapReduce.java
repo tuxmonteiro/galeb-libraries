a3b842af72bddb7f31a406e5387744aaee4e7009
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-04-30 19:29:05 -0300

    MapReduce Backend Connections updated

--- src/main/java/io/galeb/hazelcast/MapReduce.java
+++ src/main/java/io/galeb/hazelcast/MapReduce.java
@@ -1,22 +1,33 @@
 package io.galeb.hazelcast;
 
-import java.util.UUID;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import io.galeb.core.model.Metrics;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICompletableFuture;
 import com.hazelcast.core.IMap;
+import com.hazelcast.mapreduce.Combiner;
+import com.hazelcast.mapreduce.CombinerFactory;
+import com.hazelcast.mapreduce.Context;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobTracker;
+import com.hazelcast.mapreduce.KeyValueSource;
+import com.hazelcast.mapreduce.Mapper;
+import com.hazelcast.mapreduce.Reducer;
+import com.hazelcast.mapreduce.ReducerFactory;
 
 public class MapReduce {
 
-    public static final String MAP_ID = UUID.randomUUID().toString();
+    public static final String MAP_ID = MapReduce.class.getSimpleName();
 
     private final HazelcastInstance hazelcastInstance;
 
     private final IMap<String, Integer> mapBackendConn;
 
-    private Long timeOut = 60000L;
+    private Long timeOut = 10000L;
 
     public MapReduce(final HazelcastInstance hazelcastInstance) {
         this.hazelcastInstance = hazelcastInstance;
@@ -32,19 +43,91 @@ public class MapReduce {
         return timeOut;
     }
 
-    public void addMetrics(Metrics metrics) {
-        final int metrics_total = (int) metrics.getProperties()
-                .get(Metrics.PROP_METRICS_TOTAL);
+    public void addMetrics(final Metrics metrics) {
+        final int metricsTotal = (int) metrics.getProperties()
+                                              .get(Metrics.PROP_METRICS_TOTAL);
 
-        mapBackendConn.putAsync(
-                metrics.getId(),
-                metrics_total,
-                timeOut,
-                TimeUnit.MILLISECONDS);
+        mapBackendConn.put(metrics.getId(),
+                           metricsTotal,
+                           timeOut,
+                           TimeUnit.MILLISECONDS);
     }
 
     public boolean contains(String backendId) {
         return mapBackendConn.containsKey(backendId);
+    }
+
+    public Map<String, Integer> reduce() throws InterruptedException, ExecutionException {
+
+        final JobTracker jobTracker = hazelcastInstance.getJobTracker("jobTracker1");
+        final KeyValueSource<String, Integer> source = KeyValueSource.fromMap(mapBackendConn);
+
+        final Job<String, Integer> job = jobTracker.newJob(source);
+
+        final ICompletableFuture<Map<String, Integer>> future = job.mapper(
+                new Mapper<String, Integer, String, Integer>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public void map(String key, Integer value, Context<String, Integer> context) {
+                        context.emit(key, value);
+                    }
+
+                }
+                )
+                .combiner(new CombinerFactory<String, Integer, Integer>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Combiner<Integer, Integer> newCombiner(String key) {
+                        return new Combiner<Integer, Integer>() {
+
+                            private volatile int sum;
+
+                            @Override
+                            public void combine(Integer value) {
+                                sum += value;
+                            }
+
+                            @Override
+                            public Integer finalizeChunk() {
+                                final int sum = this.sum;
+                                this.sum = 0;
+
+                                return sum;
+                            }
+                        };
+                    }
+                }
+                )
+                .reducer(new ReducerFactory<String, Integer, Integer>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Reducer<Integer, Integer> newReducer(String key) {
+                        return new Reducer<Integer, Integer>() {
+
+                            private volatile int value = 0;
+
+                            @Override
+                            public Integer finalizeReduce() {
+                                return value;
+                            }
+
+                            @Override
+                            public void reduce(Integer value) {
+                                this.value += value;
+                            }
+                        };
+                    }
+                }
+                )
+                .submit();
+
+        return future.get();
     }
 
 }
