dd2062364b00cfe883861d63b87382cc1ecdc0e9
Marcelo Monteiro <m.monteiro@corp.globo.com>
2015-06-15 16:41:56 -0300

    Flag IGNORE_XFORWARDED_FOR added

--- src/main/java/io/galeb/undertow/util/UndertowSourceIP.java
+++ src/main/java/io/galeb/undertow/util/UndertowSourceIP.java
@@ -16,6 +16,7 @@
 
 package io.galeb.undertow.util;
 
+import io.galeb.core.util.Constants;
 import io.galeb.core.util.SourceIP;
 import io.undertow.server.HttpServerExchange;
 
@@ -27,21 +28,25 @@ public class UndertowSourceIP implements SourceIP {
     public String getRealSourceIP() {
         // Morpheus: What is real? How do you define 'real'?
 
+        String sourceIP = null;
+
         if (exchange == null) {
             return DEFAULT_SOURCE_IP;
         }
 
-        String sourceIP = exchange.getRequestHeaders().getFirst(HTTP_HEADER_XREAL_IP);
-        if (sourceIP!=null) {
-            return sourceIP;
-        }
+        if (IGNORE_XFORWARDED_FOR.isPresent() && !IGNORE_XFORWARDED_FOR.get().equalsIgnoreCase(Constants.FALSE)) {
+            sourceIP = exchange.getSourceAddress().getHostString();
+        } else {
+            sourceIP = exchange.getRequestHeaders().getFirst(HTTP_HEADER_XREAL_IP);
+            if (sourceIP!=null) {
+                return sourceIP;
+            }
+            sourceIP = exchange.getRequestHeaders().getFirst(HTTP_HEADER_X_FORWARDED_FOR);
+            if (sourceIP!=null) {
+                return sourceIP.split(",")[0];
+            }
 
-        sourceIP = exchange.getRequestHeaders().getFirst(HTTP_HEADER_X_FORWARDED_FOR);
-        if (sourceIP!=null) {
-            return sourceIP.split(",")[0];
         }
-
-        sourceIP = exchange.getSourceAddress().getHostString();
         if (sourceIP!=null) {
             return sourceIP;
         }
